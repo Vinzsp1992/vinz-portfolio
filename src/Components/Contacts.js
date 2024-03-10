c88a0df1b912b07b940b24cf9e3950e9a399a9ea
Vinz <vincentsanpedro2015@gmail.com>
2024-03-11 01:13:02 +0800

    Update Contacts.js

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -52,7 +52,7 @@ const Contacts = () => {
   };
 
   return (
-    <div id="contact" className='pb-10 h-[105vh] bg-slate-200 bg-no-repeat bg-cover pt-12'>
+    <div id="contact" className='pb-10 lg:h-[115vh] h-[105vh] bg-slate-200 bg-no-repeat bg-cover pt-12'>
         <h1 className='text-center lg:text-left lg:ml-16 text-3xl font-bold text-slate-200 '>
         <ion-icon name="mail-outline"></ion-icon>CONTACT<span className='text-yellow-300'> US</span></h1>
         
