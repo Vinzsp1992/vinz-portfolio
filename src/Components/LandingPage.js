99283ccb2fba7494e94ef27ff0850800d300edee
Kamnida1992 <vincentsanpedro2015@gmail.com>
2024-05-17 06:32:31 +0800

    Updated UI

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -24,12 +24,12 @@ const LandingPage = () => {
             <div className='bg-slate-200 bg-opacity-10 rounded-br-full rounded-tl-full
             rounded-tr-3xl w-[auto] p-2 h-[100%] lg:h-[500px] shadow-xl
             pb-10 lg:w-[50%] lg:p-5'>
-                <h1 className='text-white font-semibold text-3xl mt-5 mx-5'>Hello, I Am </h1>
+                <h1 className='text-white font-semibold text-3xl mt-5 mx-5'>Hello, I am </h1>
                 <h1 className='text-slate-200 text-2xl lg:text-4xl mb-4 mx-5 font-bold
                 animate-typing overflow-hidden whitespace-nowrap'>Vincent
                 <span className='text-sky-500'> San Pedro</span></h1>
                 <div className='mx-5'>
-                <h2 className='text-slate-100 italic'>Welcome to my portfolio, i Am a web developer
+                <h2 className='text-slate-100 italic'>Welcome to my portfolio, i am a web developer
                 who is passionate about translating creative visions into seamless digital experiences.<br/><br/>
                 I wanted to share with you my portfolio, which reflects my dedication to the world of web development and showcases the skills and projects I've been working on.<br/><br/>
                 
